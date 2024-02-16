beeb901b62a7c5a07ea6a68c34ae22ba53250933
Kedar <kedark@mkcl.org>
2024-02-16 15:10:31 +0530

    viteconfig fix

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: process.env.NODE_ENV =  "/auction" ,
-  base: process.env.NODE_ENV = "/auction",
+  publicDir: process.env.NODE_ENV =  "/auction/" ,
+  base: process.env.NODE_ENV = "/auction/",
 
   resolve: {
     alias: {
