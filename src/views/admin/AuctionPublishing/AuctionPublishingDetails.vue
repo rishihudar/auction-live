1950c01358abb49ce2ab3cb41960324e2b3c912a
bhushanp <bhushanp@mkcl.org>
2024-03-02 00:55:39 +0530

    Design the Auction Publishing Details

--- src/views/admin/AuctionPublishing/AuctionPublishingDetails.vue
+++ src/views/admin/AuctionPublishing/AuctionPublishingDetails.vue
@@ -1,95 +1,275 @@
 <template>
   <div class="card">
-    <DataTable
-      v-model:expandedRows="expandedRows"
-      :value="auctionData"
-      showGridlines
-      tableStyle="min-width: 50rem"
-    >
-    <template #empty>No Auctions Found</template>
-      <Column field="auctionCode" header="Auction Code"> </Column>
-      <Column field="auctionDescription" header="Auction Description"> </Column>
-      <Column field="inventoryCategoryName" header="Auction Category"> </Column>
-      <Column field="eventProcessingFees" header="Auction Fees"> </Column>
-      <Column
-        field="startDate"
-        header="Processing Fee StartDate/Time"
-      >
-      </Column>
-      <Column field="endDate" header="Processing Fee EndDate/Time">
-      </Column>
-      <Column expander header="Action" style="width: 5rem" >
-      <template #rowtogglericon="">
-<Button label="Action"/>
-      </template>
-      </Column>
-      <template #expansion="slot">
-        <div class="card">
-          <h2>
-            Auction Code: {{ slot.data.auctionCode }} Auction Category:
-            {{ slot.data.inventoryCategoryName }} Description:{{
-              slot.data.auctionDescription
-            }}
-            Auction Type:{{ slot.data.auctionType }}
-          </h2>
-          <h2>
-            Auction Method: {{ slot.data.auctionMethodName }} Auction Process:
-            {{ slot.data.auctionProcessName }} Department:
-            {{ slot.data.departmentName }} District: {{ slot.data.district }} MC:
-            {{ slot.data.mcName }}
-          </h2>
-          <h2>
-            Location: {{ slot.data.location }} Area: {{ slot.data.AREA }}
-          </h2>
-          <Button label="View Publishing Details" @click="viewPublishDetails(slot.data.auctionCode),fetchAllStepsAuctionPreview(),visible = true" />
-          <Dialog
-            v-model:visible="visible"
-            modal
-            header="Publish Auction"
-            :style="{ width: '60rem' }"
-          >
-            <h2>Auction Code: {{ auctionId }}</h2>
-            <label for="calendar-12h" class="font-bold block mb-2">
-              Processing Fee And EMD payment Start Date:</label
+    <div class="table-custom">
+        <DataTable
+        v-model:expandedRows="expandedRows"
+        :value="auctionData"
+        showGridlines
+        tableStyle="min-width: 50rem"
+        >
+        <template #empty>No Auctions Found</template>
+        <Column field="auctionCode" header="Auction Code"> </Column>
+        <Column field="auctionDescription" header="Auction Description"> </Column>
+        <Column field="inventoryCategoryName" header="Auction Category"> </Column>
+        <Column field="eventProcessingFees" header="Auction Fees"> </Column>
+        <Column
+            field="startDate"
+            header="Processing Fee StartDate/Time"
+        >
+        </Column>
+        <Column field="endDate" header="Processing Fee EndDate/Time">
+        </Column>
+        <Column expander header="Action" style="width: 5rem" >
+        <template #rowtogglericon="">
+            <fa-webhook></fa-webhook>
+            Action            
+        </template>
+        </Column>
+        <template #expansion="slot">
+            <div class="box-section">
+                <div class="bs-header">
+                    Auction Description
+                    <div class="bs-action">
+                        <Button severity="secondary" class="btn-sm"  @click="viewPublishDetails(slot.data.auctionCode),fetchAllStepsAuctionPreview(),visible = true">
+                            <fa-eye></fa-eye> View Publishing Details
+                        </Button>
+
+                        <Dialog
+                            v-model:visible="visible"
+                            modal
+                            header="Publish Auction"
+                            :style="{ width: '60rem' }"
+                        >
+                            <div class="modal-subtitle">Auction Code: <span> {{ auctionId }}</span> </div>
+                            <div class="form-grid">
+                                <div class="col-span-full md:col-span-6">
+                                    <div class="fm-group">
+                                        <label class="fm-label" for="Processing Fee">Processing Fee And EMD payment Start Date:</label>                                            
+                                        <div class="fm-inner">
+                                            <Calendar
+                                            id="calendar-24h"
+                                            v-model="selectedStartDate"
+                                            showTime
+                                            hourFormat="24"
+                                            :minDate="minDate"
+                                            :showIcon="true"
+                                            />
+                                        </div>
+                                        <div class="fm-info">
+                                            {{selectedStartDate}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-span-full md:col-span-6">
+                                    <div class="fm-group">
+                                        <label class="fm-label" for="Processing Fee">Processing Fee And EMD payment End Date:</label>                                            
+                                        <div class="fm-inner">
+                                            <Calendar
+                                            id="calendar"
+                                            v-model="selectedEndDate"
+                                            showTime
+                                            hourFormat="24"
+                                            :minDate="endMinDate"
+                                            :showIcon="true"
+                                            />
+                                        </div>
+                                        <div class="fm-info">
+                                            {{selectedEndDate}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-span-full" v-if="moment(selectedEndDate).isSameOrBefore(selectedStartDate) || moment(selectedEndDate).isSame(moment(selectedStartDate),'minute')">
+                                    <label class="fm-error" for="">
+                                        Start Date should not be equal or after End Date !
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- <label for="calendar-12h" class="font-bold block mb-2">
+                            Processing Fee And EMD payment Start Date:</label
+                            >
+                            <Calendar
+                            id="calendar-24h"
+                            v-model="selectedStartDate"
+                            showTime
+                            hourFormat="24"
+                            :minDate="minDate"
+                            :showIcon="true"
+                            />
+                            {{selectedStartDate}}
+                            <label for="calendar-12h" class="font-bold block mb-2">
+                            Processing Fee And EMD payment End Date:</label
+                            >
+                            <Calendar
+                            id="calendar"
+                            v-model="selectedEndDate"
+                            showTime
+                            hourFormat="24"
+                            :minDate="endMinDate"
+                            :showIcon="true"
+                            />
+                            {{selectedEndDate}}
+                            <div v-if="moment(selectedEndDate).isSameOrBefore(selectedStartDate) || moment(selectedEndDate).isSame(moment(selectedStartDate),'minute')" style="color: red;">
+                            Start Date should not be equal or after End Date !
+                            </div>
+                            <div>
+                                    <input type="checkbox" id="agreeCheckbox" v-model="agree" />
+                            <label for="agreeCheckbox">  </label></div>--> 
+                            <div class="col-span-full">
+                                <div class="fm-group">
+                                    <div class="fm-check-holder fm-check-center">
+                                        <div class="fm-checkbox">
+                                            <input type="checkbox" id="agreeCheckbox" v-model="agree" />
+                                            <label for="agreeCheckbox">I agree that to publish.  </label>
+                                            <!-- <Checkbox
+                                            v-model="agree"
+                                            :binary="true"
+                                            inputId="agreeCheckbox"
+                                            required
+                                            />
+                                            <label for="agreeCheckbox">I agree that to publish.</label> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-action">             
+                            <Button
+                                type="button"
+                                label="Publish"
+                                :disabled="!agree"
+                                @click="publishAuction(),visible=false"
+                            ></Button>
+                            </div>
+                        </Dialog>
+                    </div>        
+                </div>
+                <div class="bs-item-holder">
+                    <div class="bs-item col-span-6">
+                        <div class="bs-label">Auction Code:</div>
+                        <div class="bs-value">
+                            {{ slot.data.auctionCode }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-6">
+                        <div class="bs-label">Auction Category:</div>
+                        <div class="bs-value">
+                            {{ slot.data.inventoryCategoryName }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-6">
+                        <div class="bs-label">Description:</div>
+                        <div class="bs-value">
+                            {{ slot.data.auctionDescription }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-6">
+                        <div class="bs-label">Auction Method:</div>
+                        <div class="bs-value">
+                            {{ slot.data.auctionMethodName }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">Auction Process:</div>
+                        <div class="bs-value">
+                            {{ slot.data.auctionProcessName }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">Department:</div>
+                        <div class="bs-value">
+                            {{ slot.data.departmentName }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">District:</div>
+                        <div class="bs-value">
+                            {{ slot.data.district }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">MC:</div>
+                        <div class="bs-value">
+                            {{ slot.data.mcName }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">Location:</div>
+                        <div class="bs-value">
+                            {{ slot.data.location }}
+                        </div>
+                    </div>
+                    <div class="bs-item col-span-4">
+                        <div class="bs-label">Area:</div>
+                        <div class="bs-value">
+                            {{ slot.data.AREA }}
+                        </div>
+                    </div>
+                </div>
+            <!-- <h2>
+                Auction Code: {{ slot.data.auctionCode }} Auction Category:
+                {{ slot.data.inventoryCategoryName }} Description:{{
+                slot.data.auctionDescription
+                }}
+                Auction Type:{{ slot.data.auctionType }}
+            </h2>
+            <h2>
+                Auction Method: {{ slot.data.auctionMethodName }} Auction Process:
+                {{ slot.data.auctionProcessName }} Department:
+                {{ slot.data.departmentName }} District: {{ slot.data.district }} MC:
+                {{ slot.data.mcName }}
+            </h2>
+            <h2>
+                Location: {{ slot.data.location }} Area: {{ slot.data.AREA }}
+            </h2> 
+            <Button label="View Publishing Details" @click="viewPublishDetails(slot.data.auctionCode),fetchAllStepsAuctionPreview(),visible = true" />
+            <Dialog
+                v-model:visible="visible"
+                modal
+                header="Publish Auction"
+                :style="{ width: '60rem' }"
             >
-            <Calendar
-              id="calendar-24h"
-              v-model="selectedStartDate"
-              showTime
-              hourFormat="24"
-              :minDate="minDate"
-              :showIcon="true"
-            />
-            {{selectedStartDate}}
-            <label for="calendar-12h" class="font-bold block mb-2">
-              Processing Fee And EMD payment End Date:</label
-            >
-            <Calendar
-              id="calendar"
-              v-model="selectedEndDate"
-              showTime
-              hourFormat="24"
-              :minDate="endMinDate"
-              :showIcon="true"
-            />
-            {{selectedEndDate}}
-            <div v-if="moment(selectedEndDate).isSameOrBefore(selectedStartDate) || moment(selectedEndDate).isSame(moment(selectedStartDate),'minute')" style="color: red;">
-             Start Date should not be equal or after End Date !
+                <h2>Auction Code: {{ auctionId }}</h2>
+                <label for="calendar-12h" class="font-bold block mb-2">
+                Processing Fee And EMD payment Start Date:</label
+                >
+                <Calendar
+                id="calendar-24h"
+                v-model="selectedStartDate"
+                showTime
+                hourFormat="24"
+                :minDate="minDate"
+                :showIcon="true"
+                />
+                {{selectedStartDate}}
+                <label for="calendar-12h" class="font-bold block mb-2">
+                Processing Fee And EMD payment End Date:</label
+                >
+                <Calendar
+                id="calendar"
+                v-model="selectedEndDate"
+                showTime
+                hourFormat="24"
+                :minDate="endMinDate"
+                :showIcon="true"
+                />
+                {{selectedEndDate}}
+                <div v-if="moment(selectedEndDate).isSameOrBefore(selectedStartDate) || moment(selectedEndDate).isSame(moment(selectedStartDate),'minute')" style="color: red;">
+                Start Date should not be equal or after End Date !
+                </div>
+                <div> <input type="checkbox" id="agreeCheckbox" v-model="agree" required/>
+                <label for="agreeCheckbox">  I agree that to publish.</label></div>
+                <div class="flex justify-content-end gap-2">             
+                <Button
+                    type="button"
+                    label="Publish"
+                    :disabled="!agree"
+                    @click="publishAuction(),visible=false"
+                ></Button>
+                </div>
+            </Dialog>-->
             </div>
-            <div> <input type="checkbox" id="agreeCheckbox" v-model="agree" required/>
-              <label for="agreeCheckbox">  I agree that to publish.</label></div>
-            <div class="flex justify-content-end gap-2">             
-              <Button
-                type="button"
-                label="Publish"
-                :disabled="!agree"
-                @click="publishAuction(),visible=false"
-              ></Button>
-            </div>
-          </Dialog>
-        </div>
-      </template>
-    </DataTable>
+        </template>
+        </DataTable>
+    </div>
   </div>
 </template>
 
@@ -102,6 +282,10 @@ import Calendar from "primevue/calendar";
 import moment from "moment";
 import MQL from "@/plugins/mql.js";
 import { createToaster } from "@meforma/vue-toaster";
+
+import faEye from '../../../../assets/icons/eye.svg'
+import faWebhook from '../../../../assets/icons/webhook.svg'
+
 const toaster = createToaster({ position: "top-right", duration: 5000 });
 
 const auctionData = ref([]);
