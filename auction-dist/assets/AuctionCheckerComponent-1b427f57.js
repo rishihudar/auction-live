b1da62e20add6cff82a5ce989a1bb6ae06f48cb4
pavan <pavanjadhav591@gmail.com>
2024-03-05 15:43:36 +0530

    changes in main.js according to local requirement

--- auction-dist/assets/AuctionCheckerComponent-1b427f57.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as n,e as o}from"./index-4336d1e5.js";const r=e({},[["render",function(e,r){return n(),o("h1",null," AuctionCheckerComponent ")}],["__file","D:/acution-live/auction-server/auction-live/src/views/Dashboard/AuctionCheckerComponent.vue"]]);export{r as default};
-//# sourceMappingURL=AuctionCheckerComponent-1b427f57.js.map
