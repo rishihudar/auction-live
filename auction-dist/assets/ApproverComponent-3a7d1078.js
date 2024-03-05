ec49d177dec57098bbb5443981d81d6984d244ac
Shweta Pandey <shwetapa@mkcl.org>
2024-03-05 17:57:33 +0530

    changed route name ROLE_PUBLISHER into /auction-publishing-details route

--- auction-dist/assets/ApproverComponent-3a7d1078.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o,e as r}from"./index-4336d1e5.js";const n=e({},[["render",function(e,n){return o(),r("h1",null," ApproverComponent ")}],["__file","D:/acution-live/auction-server/auction-live/src/views/Dashboard/ApproverComponent.vue"]]);export{n as default};
-//# sourceMappingURL=ApproverComponent-3a7d1078.js.map
--- auction-dist/assets/SuperAdminComponent-7dc07bb7.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as n,e as o}from"./index-4336d1e5.js";const r=e({},[["render",function(e,r){return n(),o("h1",null," SuperAdminComponent ")}],["__file","D:/acution-live/auction-server/auction-live/src/views/Dashboard/SuperAdminComponent.vue"]]);export{r as default};
-//# sourceMappingURL=SuperAdminComponent-7dc07bb7.js.map
