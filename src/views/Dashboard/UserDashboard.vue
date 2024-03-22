1ba83bd99a4b5a1c2506315599963e0db24e78c1
pavan <pavanjadhav591@gmail.com>
2024-03-22 15:20:45 +0530

    added role wise dashboard in auction live

---
+++ src/views/Dashboard/UserDashboard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="stat-area stat-area-alt">
+        <div class="card-wrapper" v-for="(card, index) in cards" :key="index"  @click="navigateToAbout">
+          <div class="card card-stat card-stat-alt"  v-if="userRole==card.roleId">
+            <div class="card-body">
+              <h4 class="card-value">{{ card.countQuery }}</h4>
+              <div class="card-name-holder">
+                <b class="fw-medium card-name">{{card.card}}</b>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from "vue";
+import { login } from "../../store/modules/login";
+import MQL from "@/plugins/mql.js";
+
+const loginStore = login();
+const userRole = ref();
+const cards = ref([]);
+
+function FetchUserRole() {
+  console.log("Role-", loginStore.currentRole.roleId);
+  userRole.value = loginStore.currentRole.roleId;
+}
+
+const publishAuctionsCount = ref(0);
+function navigateToAbout() {
+  console.log("clicked");
+}
+
+function fetchDashboardDetails() {
+  new MQL()
+    .useManagementServer()
+    .setActivity("o.[FetchDashboardAndRoleDetails]")
+    .setData({userRoleId:userRole.value,
+        entityId:loginStore.entityId,
+        organizationId:loginStore.organizationId,
+        userId:loginStore.loginId
+    })
+    .fetch()
+    .then((rs) => {
+      let res = rs.getActivity("FetchDashboardAndRoleDetails", true);
+      if (rs.isValid("FetchDashboardAndRoleDetails")) {
+        cards.value = res.result.dashboardDetails;
+        console.log("cards", cards.value);
+      } else {
+        rs.showErrorToast("FetchDashboardAndRoleDetails");
+      }
+    });
+}
+
+onBeforeMount(() => {
+  FetchUserRole();
+  fetchDashboardDetails();
+});
+</script>
