be221889dbf73e39400d33aa19bdcd1f5f0c7822
pavan <pavanjadhav591@gmail.com>
2024-04-23 18:28:29 +0530

    added user dashboard

--- src/router.js
+++ src/router.js
@@ -144,7 +144,7 @@ const router = createRouter({
     },
     {
       path: "/add-Dashboard",
-      name: "ROLE_SCHEDULER",
+      name: "ROLE_SUPERADMIN",
       component: loadDashboardView("AddDashboardUI"),
       meta: { title: "UserDashboard", lang: "en", icon: "mdi mdi-home-outline" , roles: ['ROLE_MAKER','ROLE_CHECKER','ROLE_APPROVER','ROLE_SCHEDULER']},
     },
