f6c9c00fb8105806665d78cf64326bdf8fd266a2
Kedar <kedark@mkcl.org>
2024-06-25 15:16:14 +0530

    Merge conflict resolve(Scheduler Button)

--- src/components/SchedulerButton.vue
+++ src/components/SchedulerButton.vue
@@ -179,7 +179,15 @@ async function schedule() {
 }
 
 function formatDate(date) {
-  return moment(date).format('MM/DD/YYYY HH:mm');
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+  const seconds = ("0" + date.getSeconds()).slice(-2);
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  //return moment(date).format('MM/DD/YYYY HH:mm');
 }
 
 function NotifyAuctionScheduledAndPasscodes(){
